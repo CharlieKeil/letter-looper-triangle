cf5dfa02b7c78e66cd580b18405daf2d771cf4d5
CharlieKeil <Keil0009@algonquinlive.com>
2016-02-28 18:51:10 -0500

    Added all files.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var letter = prompt('Enter a Letter');
+var i;
+
+for (i = 0; i < 1; i++) {
+  document.write(letter + '<br>');
+  document.write(letter + letter + '<br>');
+  document.write(letter + letter + letter +'<br>');
+  document.write(letter + letter + letter + letter +'<br>');
+  document.write(letter + letter + letter + letter + letter +'<br>');
+  document.write(letter + letter + letter + letter + letter + letter + '<br>');
+  document.write(letter + letter + letter + letter + letter + letter + letter + '<br>');
+  document.write(letter + letter + letter + letter + letter + letter + letter + letter + '<br>');
+  document.write(letter + letter + letter + letter + letter + letter + letter + letter + letter + '<br>');
+  document.write(letter + letter + letter + letter + letter + letter + letter + letter + letter + letter +'<br>');
+}
